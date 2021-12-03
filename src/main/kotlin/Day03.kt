da34a96e8660910546b6f332790325052ffa76b0
George Kokinis <george.kokinis@gmail.com>
2021-12-03 07:27:15 +0000

    Refactor away from a map; implement Part 2.

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,13 +1,13 @@
-fun List<String>.toCols(): Map<Int, MutableList<Char>> {
-    val lineSize = this[0].length;
-    val cols = mutableMapOf<Int, MutableList<Char>>()
+fun List<String>.toCols(): List<MutableList<Char>> {
+    val lineSize = this[0].length
+    val cols : MutableList<MutableList<Char>> = mutableListOf()
     for (i in 0 until lineSize) {
-        cols[i] = mutableListOf()
+        cols.add(mutableListOf())
     }
     for (line in this) {
         for (i in 0 until lineSize) {
             val col = cols[i]
-            col?.add(line[i])
+            col.add(line[i])
         }
     }
     return cols
@@ -17,18 +17,36 @@ fun binaryDiagnostics(lines: List<String>): Int {
     val cols = lines.toCols()
     var gamma = ""; var epsilon = ""
     for (col in cols){
-        gamma += col.value.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
-        epsilon += col.value.groupingBy { it }.eachCount().minByOrNull { it.value }?.key
+        gamma += col.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
+        epsilon += col.groupingBy { it }.eachCount().minByOrNull { it.value }?.key
     }
     return gamma.padStart(16, '0').toInt(2) * epsilon.padStart(16, '0').toInt(2)
 }
 
 fun oxygenGenRating(lines: List<String>): Int {
-
+    val working = lines.toMutableList()
+    for (i in working.indices) {
+        if (working.size == 1) break
+        // Don't know what this does on equal values.
+        val d = working.toCols()[i].groupingBy { it }.eachCount()
+        val c: Char? = if (d['0'] == d['1']) '1'
+        else d.maxByOrNull { it.value }?.key
+        working.retainAll { it[i] == c }
+    }
+    return working[0].padStart(16, '0').toInt(2)
 }
 
 fun co2ScrubRating(lines: List<String>): Int {
-
+    val working = lines.toMutableList()
+    for (i in working.indices) {
+        if (working.size == 1) break
+        // Don't know what this does on equal values.
+        val d = working.toCols()[i].groupingBy { it }.eachCount()
+        val c: Char? = if (d['0'] == d['1']) '0'
+        else d.minByOrNull { it.value }?.key
+        working.retainAll { it[i] == c }
+    }
+    return working[0].padStart(16, '0').toInt(2)
 }
 
 fun lifeSupportRating(lines: List<String>): Int {
