2a8a12880ff9899c8248a8a207d6a916d6d4b42e
George Kokinis <george.kokinis@gmail.com>
2021-12-08 08:47:38 +0000

    Day4: Classes.

--- src/main/kotlin/Day4/Day04.kt
+++ src/main/kotlin/Day4/Day04.kt
@@ -1,3 +1,15 @@
 package Day4
-
 import readInput
+
+
+class Point(val value: Int, val called: Boolean)
+
+class BingoBoard(BoardString: List<String>) {
+    var board: List<List<Point>> = BoardString.map { it.trim().split("  ", " ").map { Point(it.toInt(), false) } }
+}
+
+fun main() {
+    val lines = readInput("Day4", "Day4Test")
+    var b = BingoBoard(lines.slice(3..6))
+    println(lines.size)
+}
